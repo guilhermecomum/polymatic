5a4fe277f2668ddebada34ea59dab89e5583bae0
Guilherme Guerra <guilherme.ga@gmail.com>
2020-03-21 19:24:42 -0300

    Fix version on header

--- src/Header.js
+++ src/Header.js
@@ -38,7 +38,7 @@ function Header({
     <div className="header">
       <div>
         <h1 className="header-logo">
-          Polymatic <small>v3.3</small>
+          Polymatic <small>v3.0.3</small>
         </h1>
       </div>
       <div className="header-controls">
