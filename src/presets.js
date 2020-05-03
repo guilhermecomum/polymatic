88ef50d3faf872750a5c39b001364abf1d713d97
Guilherme Guerra <guilherme.ga@gmail.com>
2020-05-03 18:25:38 -0300

    Adds new presets

--- src/presets.js
+++ src/presets.js
@@ -2,24 +2,29 @@ import shortid from "shortid";
 
 const presets = {
   [shortid.generate()]: {
-    name: "Rumba",
-    instruments: [
-      { sequence: "1001000100101000", tempo: 120, sample: "conga1" }
-    ]
+    name: "Toque Ignbin",
+    instruments: [{ sequence: "101101011010", tempo: 120, sample: "conga1" }]
   },
   [shortid.generate()]: {
-    name: "Centro de gravidade",
+    name: "Son cubano",
     instruments: [
       {
         sequence: "10101",
         tempo: 120,
-        sample: "bumbo"
-      },
-      {
-        sequence: "10110101",
-        tempo: 120,
-        sample: "bass"
+        sample: "claves"
       }
+    ]
+  },
+  [shortid.generate()]: {
+    name: "Rumba",
+    instruments: [
+      { sequence: "1001001001001000", tempo: 120, sample: "claves" }
+    ]
+  },
+  [shortid.generate()]: {
+    name: "Samba",
+    instruments: [
+      { sequence: "1011010101101010", tempo: 60, sample: "tamborim" }
     ]
   }
 };
