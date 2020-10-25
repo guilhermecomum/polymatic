6225af1c5a611ffd38b1c4d9506aefbe571ea0c0
Guilherme Guerra <guilherme.ga@gmail.com>
2020-10-25 17:31:06 -0300

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Polymatic
-
-Polymatic is a tool create to lear how to work with poly[rhythm|metric|mindset].
-Think as a modern circular music sheet. You can use as like nice beatmachine, but the main pourpose is learn how to play polymetric and polyrhythm.
-
-Polymatic is heavy-based on [Ehsan Ziya's](https://github.com/zya) [beet.js](https://github.com/zya/beet.js).
-
-Have fun!
---
+++ precache-manifest.5522678787685d068f1094f40af18a61.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d20e5f1488c76f63950d3c541583d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cdfa2e1e0c6231fbafe",
+    "url": "/static/css/2.d219d102.chunk.css"
+  },
+  {
+    "revision": "411ee8aec0c570bd9260",
+    "url": "/static/css/main.c5a13c2c.chunk.css"
+  },
+  {
+    "revision": "0cdfa2e1e0c6231fbafe",
+    "url": "/static/js/2.514cb468.chunk.js"
+  },
+  {
+    "revision": "a918aa92163ce03f9a552a34e1f5ed09",
+    "url": "/static/js/2.514cb468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411ee8aec0c570bd9260",
+    "url": "/static/js/main.eb5b41f2.chunk.js"
+  },
+  {
+    "revision": "df1df27a039b147a1dd9",
+    "url": "/static/js/runtime-main.f0083627.js"
+  },
+  {
+    "revision": "1065034eb964bc28cb931060324ab7fb",
+    "url": "/static/media/agogo1.1065034e.wav"
+  },
+  {
+    "revision": "0e4e658fda61daee5e10a4d4a0caec7d",
+    "url": "/static/media/agogo2.0e4e658f.wav"
+  },
+  {
+    "revision": "b2419d4fafe1cb20089a8abc01d465ab",
+    "url": "/static/media/bass.b2419d4f.wav"
+  },
+  {
+    "revision": "3753c44b417f204ef9527a4c7fd249c1",
+    "url": "/static/media/block.3753c44b.wav"
+  },
+  {
+    "revision": "d9f8c3a9c78d3dd49d41c41b09852d90",
+    "url": "/static/media/block1.d9f8c3a9.wav"
+  },
+  {
+    "revision": "7e7a9a5942dca875c27fc30c417eb50f",
+    "url": "/static/media/block2.7e7a9a59.wav"
+  },
+  {
+    "revision": "4c11d5fa535a7a76f96290b55f19ffbc",
+    "url": "/static/media/bum.4c11d5fa.wav"
+  },
+  {
+    "revision": "f6ddaabb61f3248e6412ec5821e96d37",
+    "url": "/static/media/caxixi.f6ddaabb.wav"
+  },
+  {
+    "revision": "7bdee98b4a5583027d899780908b1247",
+    "url": "/static/media/clap.7bdee98b.wav"
+  },
+  {
+    "revision": "400c9ef37e02e6807b2d81de59d0a6b2",
+    "url": "/static/media/claves.400c9ef3.wav"
+  },
+  {
+    "revision": "41b2f57fae93e093d780c585b6c9007f",
+    "url": "/static/media/conga1.41b2f57f.wav"
+  },
+  {
+    "revision": "5cc86b7125aecff57058a005288e8a89",
+    "url": "/static/media/conga2.5cc86b71.wav"
+  },
+  {
+    "revision": "435be93fc13be1399f4741416792cf0a",
+    "url": "/static/media/conga3.435be93f.wav"
+  },
+  {
+    "revision": "a38157fd77083267a5e436926e44bd3c",
+    "url": "/static/media/conga4.a38157fd.wav"
+  },
+  {
+    "revision": "566cddac7c8092e10386c8c85908d82a",
+    "url": "/static/media/cowbell.566cddac.wav"
+  },
+  {
+    "revision": "950d832bfee0164cd8756d73876ed4b1",
+    "url": "/static/media/cymbal.950d832b.wav"
+  },
+  {
+    "revision": "f3e648cbe6fa1f039ecd00253f568260",
+    "url": "/static/media/djembe1.f3e648cb.wav"
+  },
+  {
+    "revision": "d38ab3d2a59f69a02cc9ccc2e944d90d",
+    "url": "/static/media/djembe2.d38ab3d2.wav"
+  },
+  {
+    "revision": "acef368f986ec943028abc6d7c0b24ba",
+    "url": "/static/media/djembe3.acef368f.wav"
+  },
+  {
+    "revision": "cedb0f6adb0f50ad9b8856f64b90b21f",
+    "url": "/static/media/egg1.cedb0f6a.wav"
+  },
+  {
+    "revision": "714c08892957b1a7a695b6bfb83dee39",
+    "url": "/static/media/egg2.714c0889.wav"
+  },
+  {
+    "revision": "f03a8f483d7edc9fe31c234df2b35ee3",
+    "url": "/static/media/hat.f03a8f48.wav"
+  },
+  {
+    "revision": "a315148ba0ec228af8cddce214c2e928",
+    "url": "/static/media/hit.a315148b.wav"
+  },
+  {
+    "revision": "950d832bfee0164cd8756d73876ed4b1",
+    "url": "/static/media/hithat.950d832b.wav"
+  },
+  {
+    "revision": "9621794a8325ce564397889d122a2062",
+    "url": "/static/media/kick1.9621794a.wav"
+  },
+  {
+    "revision": "d96070b851a4c286ccea741a80475aa4",
+    "url": "/static/media/kick1.d96070b8.wav"
+  },
+  {
+    "revision": "18445a5edf9bad50b459d308aee1c270",
+    "url": "/static/media/kick2.18445a5e.wav"
+  },
+  {
+    "revision": "c09eab149334a4ba3927fad81bfdc009",
+    "url": "/static/media/kicklow.c09eab14.wav"
+  },
+  {
+    "revision": "c392cef6a220c87c9f83664d3498c233",
+    "url": "/static/media/kik2.c392cef6.wav"
+  },
+  {
+    "revision": "557e021083d64e256f479bbaaf1856ab",
+    "url": "/static/media/kik3.557e0210.wav"
+  },
+  {
+    "revision": "4707aa1f66ae3cb250d78a950ba4faac",
+    "url": "/static/media/snare.4707aa1f.wav"
+  },
+  {
+    "revision": "ac6ee711a42b10498c65658d342ab6c4",
+    "url": "/static/media/snare.ac6ee711.wav"
+  },
+  {
+    "revision": "4c11d5fa535a7a76f96290b55f19ffbc",
+    "url": "/static/media/surdo1.4c11d5fa.wav"
+  },
+  {
+    "revision": "de3c5a5c558e2f1e2529abfe705aef42",
+    "url": "/static/media/surdo2.de3c5a5c.wav"
+  },
+  {
+    "revision": "d327f3d9376af7035f9144066721ef47",
+    "url": "/static/media/surdo3.d327f3d9.wav"
+  },
+  {
+    "revision": "5e1800e912c401f8dac73a456c9f38e0",
+    "url": "/static/media/surdo4.5e1800e9.wav"
+  },
+  {
+    "revision": "a6cbfb587e989be28f08a6ba36c31057",
+    "url": "/static/media/tamborim.a6cbfb58.wav"
+  },
+  {
+    "revision": "a6cbfb587e989be28f08a6ba36c31057",
+    "url": "/static/media/tamborim1.a6cbfb58.wav"
+  },
+  {
+    "revision": "06bd97bb393fd51c02e0ba2ee3c8d456",
+    "url": "/static/media/tamborim2.06bd97bb.wav"
+  },
+  {
+    "revision": "c6ac1d9b1b3fb80e980c2056a13d1eab",
+    "url": "/static/media/xequere1.c6ac1d9b.wav"
+  },
+  {
+    "revision": "0e563d7aae5c15e8c052137b8768cc35",
+    "url": "/static/media/xequere2.0e563d7a.wav"
+  }
+]);
