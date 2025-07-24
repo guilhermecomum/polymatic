945c76993575d4318105279aad92acdfb45ea15a
Guilherme Guerra <guilherme.ga@gmail.com>
2025-07-23 22:19:15 -0300

    Adds bpm control

--- remix/app/components/button.tsx
+++ remix/app/components/button.tsx
@@ -1,3 +1,5 @@
+import { cx } from "~/framework/utils";
+
 function Button({
   children,
   className,
@@ -5,7 +7,10 @@ function Button({
 }: JSX.IntrinsicElements["button"]) {
   return (
     <button
-      className={`bg-red-500 hover:bg-red-700 p-2 text-sm text-white ${className}`}
+      className={cx(
+        `bg-red-500 hover:bg-red-700 p-2 text-sm text-white`,
+        className,
+      )}
       {...props}
     >
       {children}
